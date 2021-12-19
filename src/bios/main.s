3184185d4eddc626002d6f29dd2c7dfc97abdf20
anjennings <anjennings@wisc.edu>
2021-12-19 17:20:02 -0600

    Filled in vector table

--- src/bios/main.s
+++ src/bios/main.s
@@ -42,6 +42,49 @@
 .long	INIT_SP		; Initial stack pointer
 .long	RESET_V		; Initial PC
 .long	GEN_V		; Bus Error
+.long 	GEN_V		; Address Error
+.long   GEN_V           ; Illegal opcode
+.long   GEN_V           ; Div by 0
+.long   GEN_V           ; CHK instruction
+.long   GEN_V           ; TRAPV
+.long   GEN_V           ; Privilege Violation
+.long   GEN_V           ; Trace
+.long   GEN_V           ; 
+.long   GEN_V           ; 
+.long   GEN_V           ; Reserved
+.long   GEN_V           ; Default interrupt vector
+.long   GEN_V           ; Reserved
+.long   GEN_V           ; Reserved
+.long   GEN_V           ; Reserved
+.long   GEN_V           ; Reserved
+.long   GEN_V           ; Reserved
+.long   GEN_V           ; Reserved
+.long   GEN_V           ; Reserved
+.long   GEN_V           ; Reserved
+.long   GEN_V           ; Spurious Int
+.long   GEN_V           ; IRQ Level 1
+.long   GEN_V           ; IRQ Level 2
+.long   GEN_V           ; IRQ Level 3
+.long   GEN_V           ; IRQ Level 4
+.long   GEN_V           ; IRQ Level 5
+.long   GEN_V           ; IRQ Level 6
+.long   GEN_V           ; IRQ Level 7
+.long   GEN_V           ; TRAP 0
+.long   GEN_V           ; TRAP 1
+.long   GEN_V           ; TRAP 2
+.long   GEN_V           ; TRAP 3
+.long   GEN_V           ; TRAP 4
+.long   GEN_V           ; TRAP 5
+.long   GEN_V           ; TRAP 6
+.long   GEN_V           ; TRAP 7
+.long   GEN_V           ; TRAP 8
+.long   GEN_V           ; TRAP 9
+.long   GEN_V           ; TRAP 10
+.long   GEN_V           ; TRAP 11
+.long   GEN_V           ; TRAP 12
+.long   GEN_V           ; TRAP 13
+.long   GEN_V           ; TRAP 14
+.long   GEN_V           ; TRAP 15
 
 
 ### --- Begin BIOS --- ###
@@ -52,7 +95,7 @@
 INIT_UART:
 
 	; Set CRA
-        MOVE.B #0x10, REG_CRA    ;0001 0000 Reset MRA
+        MOVE.B #0x10, REG_CRA   ;0001 0000 Reset MRA
 	MOVE.B #0x20, REG_CRA	;0010 0000 Reset RX
 	MOVE.B #0x30, REG_CRA	;0011 0000 Reset TX
 	MOVE.B #0x40, REG_CRA	;0100 0000 Reset Error Status
@@ -64,17 +107,17 @@ INIT_UART:
 	MOVE.B #0, REG_OPCR
 
 	; SET MRA 1/2, no parity, 8 bit, normal mode, 1 stop bit
-	;MOVE.B 0x53, REG_MRA 	;0101 0011
-	;MOVE.B 0x53, REG_MRA	;0000 0111
+	MOVE.B #0x53, REG_MRA 	;0101 0011
+	MOVE.B #0x53, REG_MRA	;0000 0111
 	
 	; Set CSRA, 38.4k baud 
-	;MOVE.B 0xC0, REG_CSRA	;1100 0000
+	MOVE.B #0xC0, REG_CSRA	;1100 0000
 
 	; Set ACR, 38.4k Baud, Counter source crystal
-	;MOVE.B 0x30, REG_ACR	;0011 0000
+	MOVE.B #0x30, REG_ACR	;0011 0000
 
 	; Set IVR to first unused vector in table
-	;MOVE.B 0xC0, REG_IMR
+	MOVE.B #0xC0, REG_IMR
 
 	; Set output port to all high but pin 7
         MOVE.B #0xFF, REG_SOP_CMD
