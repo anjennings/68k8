3dcd903ad5ca53a116ad07371395c327ce7d37ff
anjennings <anjennings@wisc.edu>
2021-12-21 15:06:46 -0600

    I'm losing my wits with this uart

--- src/bios/init.s
+++ src/bios/init.s
@@ -4,17 +4,20 @@ INIT:
         MOVE.B #0, D0
         BSR SET_INT_MASK
 
-        ; Enablue UART A
-        BSR ENABLE_UART_A
-
-        ; Clear AUX LEDs
+	; Clear AUX LEDs
         BSR INIT_OUTP
         MOVE.B #0xFF, D0
         BSR CLR_OUTP
 
-FOREVER:
+        ; Enablue UART A
+        BSR ENABLE_UART_A
 
+LOOP:
         MOVE.B #0x4D, D0
         BSR PUTC_POLL
-        JMP FOREVER
+        JMP LOOP
         RTS
+
+; Infinite Loop, useful for debug
+FOREVER:
+	JMP FOREVER
--- src/bios/uart.s
+++ src/bios/uart.s
@@ -3,27 +3,39 @@
 ### --- Output Ports --- ###
 INIT_OUTP:
         MOVE.B #0, REG_OPCR
+	NOP
         RTS
 
 # Clear bits selected by D0.b
 CLR_OUTP:
         MOVE.B D0, SOPR_CMD
+	NOP
         RTS
 
 # Set bits selected by D0.b
 SET_OUTP:
         MOVE.B D0, ROPR_CMD
+	NOP
+	RTS
 
+# Set output port to value in D0.b
+WRITE_OUTP:
+	BSR SET_OUTP
+	NOT D0
+	BSR CLR_OUTP
+	RTS
 
 ### --- Interrupts --- ###
 ; Set mask bits by D0.b
 SET_INT_MASK:
         MOVE.B D0, REG_IMR
+	NOP
         RTS
 
 ; Set interrupt vector by D0.b
 SET_INT_VECT:
         MOVE.B D0, REG_IVR
+	NOP
         RTS
 
 ### --- UART A --- ###
@@ -31,31 +43,50 @@ ENABLE_UART_A:
 
         # Reset Commands
         MOVE.B #0x20, REG_CRA   ;0001 0000 Reset RX
+	NOP
         MOVE.B #0x30, REG_CRA   ;0010 0000 Reset TX
+	NOP
         MOVE.B #0x10, REG_CRA   ;0011 0000 Reset MRA
+	NOP
         #MOVE.B #0x40, REG_CRA   ;0100 0000 Reset Error Status
 
         ; SET MRA 1/2, no parity, 8 bit, normal mode, 1 stop bit
         MOVE.B #0x13, REG_MRA   ;0001 0011
+	NOP
         MOVE.B #0x07, REG_MRA   ;0000 0111
+	NOP
 
         ; Set CSRA, 9600 baud 
         MOVE.B #0xBB, REG_CSRA  ;1011 1011
+	NOP
 
         ; Set ACR, 38.4k Baud, Counter source crystal
-        ;MOVE.B #0x30, REG_ACR   ;0011 0000
+        MOVE.B #0x30, REG_ACR   ;1011 0000
+	NOP
 
-        ; Enable TX & RX
-        MOVE.B #0x05, REG_CRA   ;0000 0101      
+        ; Enable TX
+        MOVE.B #0x04, REG_CRA   ;0000 0101
+	NOP
 
         RTS
 
 ; Print char in D0.b
 PUTC_POLL:
-        LEA REG_SRA, A0
-        MOVE.B (A0), D1
-        ANDI #0x4, D1
-        CMP.B #0x4, D1
-        BNE PUTC_POLL
-        MOVE.B D0, REG_THRA
-        RTS
+
+	MOVE.B D0, D1
+	MOVE.B #0x80, D0
+	BSR SET_OUTP
+	MOVE.B D1, D0
+
+	BTST.B #2, REG_SRA	; Could also be #3
+	NOP
+	BEQ PUTC_POLL
+	NOP
+	MOVE.B D0, REG_THRA
+	NOP
+
+	MOVE.B #0x80, D0
+	BSR CLR_OUTP
+        JMP FOREVER
+	RTS
+
